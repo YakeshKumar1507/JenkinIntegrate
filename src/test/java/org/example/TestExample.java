e65b2a04558b5a4ec4e912e165fd04ec6a948a64
YakeshSar <sarvanan9516@gmail.com>
2024-06-18 18:29:25 +0530

    Jenkin Integrate

---
+++ src/test/java/org/example/TestExample.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+    @Test
+    public void greetings(){
+        System.out.println("Welcome to scale soft");
+    }
+}
